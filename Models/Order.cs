9401412ad1c2e22bf26251263998b8fca2f5fcb4
Karuna Kandel <karuna.kandel8722@nctorontostudents.ca>
2024-11-28 08:22:49 -0500

    developing in progress

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineFashionOutletUsingMVC.Models
+{
+    public class Order
+    {
+        //Scalar properties
+        public int OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public string OrderStatus { get; set; } = "Pending"; // Default status is "Pending"
+        public double TotalAmount { get; set; }
+        public string PaymentType { get; set; }
+        public string UserName { get; set; }
+        //Navigation property
+        public User? User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineFashionOutletUsingMVC.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Role { get; set; } = "User"; // Default role is "User"
+
+       //Navigation property
+        public ICollection<Order> Orders { get; set; }
+       
+}
+    }
+
